20908cc4eef2c97ee585e50f03d57668ef05e44b
aslemammad <zorofight94@gmail.com>
2021-12-17 09:47:08 +0330

    move.js fixture

---
+++ test/fixtures/move.js
@@ -0,0 +1,10 @@
+import Tinypool from '../../dist/esm/index.js';
+import assert from 'assert';
+import { types } from 'util';
+
+export default function (moved) {
+  if (moved !== undefined) {
+    assert(types.isAnyArrayBuffer(moved));
+  }
+  return Tinypool.move(new ArrayBuffer(10));
+}
