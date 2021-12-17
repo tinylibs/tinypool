0310f28f1f58ecf11b82b7ca3453bb7b4d33640b
aslemammad <zorofight94@gmail.com>
2021-12-17 09:47:02 +0330

    publish

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,19 +1,13 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from "tsup";
 
 export default defineConfig({
   entryPoints: ['src/index.ts', 'src/worker.ts'],
-  esbuildOptions: (options) => {
-    options.banner = {
-      // js: "import { createRequire as topLevelCreateRequire } from 'module';\n const require = topLevelCreateRequire(import.meta.url);"
-      // js: "const require = import"
-    }
-  },
-  splitting: false,
+  splitting: true,
   legacyOutput: true,
-  outDir: 'dist',
-  format: ['esm'],
-  tsconfig: './tsconfig.json',
-  target: 'es2020',
+  outDir: "dist",
+  format: ["esm"],
+  tsconfig: "./tsconfig.json",
+  target: "es2020",
   clean: true,
   dts: true,
-})
+});
