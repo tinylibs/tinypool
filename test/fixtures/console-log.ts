040927a3ffe712bc80eb35d66a0c812b1a46bd8a
Ari Perkkiö <ari.perkkio@gmail.com>
2024-05-08 21:16:22 +0300

    test: remove unused fixtures

--- test/fixtures/console-log.ts
+++
@@ -1,9 +0,0 @@
-import Tinypool from '../../dist/index'
-import { resolve } from 'path'
-
-const pool = new Tinypool({
-  filename: resolve(__dirname, 'eval.js'),
-  maxThreads: 1,
-})
-
-pool.run('console.log("A"); console.log("B");')
--- test/fixtures/notify-then-sleep.ts
+++
@@ -1,5 +0,0 @@
-export default function (i32array: Int32Array) {
-  Atomics.store(i32array, 0, 1)
-  Atomics.notify(i32array, 0, Infinity)
-  Atomics.wait(i32array, 0, 1)
-}
