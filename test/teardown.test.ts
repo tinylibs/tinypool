bdc159c8ce1c67aa12a73f29cebcd68b9d6bd863
Nazar Hussain <nazarhussain@gmail.com>
2025-06-03 21:42:20 +0200

    Update teardown test

--- test/teardown.test.ts
+++ test/teardown.test.ts
@@ -2,7 +2,6 @@ import { dirname, resolve } from 'node:path'
 import { Tinypool } from 'tinypool'
 import { fileURLToPath } from 'node:url'
 import { MessageChannel } from 'node:worker_threads'
-import { isBun } from './utils'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
@@ -27,13 +26,7 @@ test('isolated workers call teardown on worker recycle', async () => {
   }
 })
 
-test('non-isolated workers call teardown on worker recycle', async ({
-  skip,
-}) => {
-  // TODO: Need to debug why `port2.off` does not behave as expected on Bun
-  // It doe not clear the handler as well as not call `unexpectedTeardown` five times
-  if (isBun) return skip('Teardown behave unexpected on bun')
-
+test('non-isolated workers call teardown on worker recycle', async () => {
   const pool = new Tinypool({
     filename: resolve(__dirname, 'fixtures/teardown.mjs'),
     minThreads: 1,
@@ -50,10 +43,9 @@ test('non-isolated workers call teardown on worker recycle', async ({
 
   const { port1, port2 } = new MessageChannel()
   port2.start()
+  port2.on('message', unexpectedTeardown)
 
   for (const index of [1, 2, 3, 4, 5]) {
-    port2.on('message', unexpectedTeardown)
-
     const transferList = index === 1 ? [port1] : []
 
     const output = await pool.run({ port: transferList[0] }, { transferList })
@@ -64,5 +56,5 @@ test('non-isolated workers call teardown on worker recycle', async ({
   const promise = new Promise((resolve) => port2.on('message', resolve))
 
   await pool.destroy()
-  await expect(promise).resolves.toMatchInlineSnapshot(`"Teardown of task #5"`)
+  await expect(promise).resolves.toEqual(`Teardown of task #5`)
 })
